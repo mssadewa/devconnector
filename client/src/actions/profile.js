8916d613331ad12d7036b5a63d9c760503151bc3
mssadewa <me@mssadewa.com>
2020-02-20 07:20:18 +0700

    Finisg Starting on Dashboard

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,18 +4,18 @@ import { setAlert } from './alert';
 import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 // Get current user profile
-export const getCurrentProfile = () => aysnc dispatch => {
-    try {
-        const res = await axios.get('/api/profile/me');
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/profile/me');
 
-        dispatch({
-            type: GET_PROFILE,
-            payload: res.data
-        })
-    } catch (error) {
-        dispatch({
-            type: PROFILE_ERROR,
-            payload: { msg: error.response.statusText, status: error.response.status }
-        })
-    }
-}
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
