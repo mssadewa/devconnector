2e3c4afe685fa616492c76a64d003df375ef28da
mssadewa <me@mssadewa.com>
2020-02-20 20:53:48 +0700

    Edit profile finish

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -5,17 +5,56 @@ import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 // Get current user profile
 export const getCurrentProfile = () => async dispatch => {
-  try {
-    const res = await axios.get('/api/profile/me');
+	try {
+		const res = await axios.get('/api/profile/me');
 
-    dispatch({
-      type: GET_PROFILE,
-      payload: res.data
-    });
-  } catch (error) {
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: error.response.statusText, status: error.response.status }
-    });
-  }
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data
+		});
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.statusText, status: error.response.status }
+		});
+	}
+};
+
+// Create or update profile
+export const createProfile = (
+	formData,
+	history,
+	edit = false
+) => async dispatch => {
+	try {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+
+		const res = await axios.post('/api/profile', formData, config);
+
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data
+		});
+
+		dispatch(setAlert(edit ? 'Profile updated' : 'Profile created'));
+
+		if (!edit) {
+			history.push('/dashboard');
+		}
+	} catch (error) {
+		const errors = error.response.data.errors;
+
+		if (errors) {
+			errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+		}
+
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: error.response.statusText, status: error.response.status }
+		});
+	}
 };
