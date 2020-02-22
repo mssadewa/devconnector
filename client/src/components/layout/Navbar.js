36b914a6304a95dbf0b71027296c7dfbe3f6944d
mssadewa <me@mssadewa.com>
2020-02-22 07:34:14 +0700

    Display Profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,7 +14,7 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         </Link>
       </li>
       <li>
-        <Link onClick={logout} href='#!'>
+        <Link onClick={logout} to='#!'>
           <i className='fas fa-sign-out-alt'></i>
           <span className='hide-sm'> Logout</span>
         </Link>
